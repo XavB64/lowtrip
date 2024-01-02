4c1b8105a89f7a90abde67d17b73ed728d1ec8e2
Clara Ni <clara.ni@student-cs.fr>
2024-01-02 23:08:11 +0100

    front: add first designed form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { Form } from "./components/Form.jsx";
 import { Map } from "./components/Map.jsx";
 import { Chart } from "./components/Chart.jsx";
 import { Container, Stack } from "@mui/material";
+import "./styles.css";
+import { NewForm } from "./components/NewForm.jsx";
 
 function App() {
   const [response, setResponse] = useState();
@@ -11,15 +13,26 @@ function App() {
 
   return (
     <Stack direction="row" className="App" style={{ height: "100vh" }}>
-      <Stack padding={3} maxWidth="25%" justifyContent="space-between">
-        <Form
-          setResponse={setResponse}
-          departure={departure}
-          setDeparture={setDeparture}
-          arrival={arrival}
-          setArrival={setArrival}
-        />
-        <Chart response={response} />
+      <Stack
+        maxWidth="40%"
+        justifyContent="space-between"
+        height="100%"
+        overflow="auto"
+      >
+        <Stack padding={3}>
+          <Stack padding={3} justifyItems="center">
+            <h1 className="title">Compare the emissions from your travels</h1>
+          </Stack>
+          <NewForm />
+          {/* <Form
+            setResponse={setResponse}
+            departure={departure}
+            setDeparture={setDeparture}
+            arrival={arrival}
+            setArrival={setArrival}
+          /> */}
+          <Chart response={response} />
+        </Stack>
       </Stack>
       <Container style={{ padding: 0 }}>
         <Map response={response} departure={departure} arrival={arrival} />
