ea8c457cd49f9acba2a754028c36d68c56d786e9
Clara Ni <clara.ni@outlook.fr>
2024-02-06 21:01:55 +0100

    backend: clean app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-#Librairies
-from flask import Flask, request, render_template, send_from_directory, json
+# Librairies
+from flask import Flask, request, json
 from backend import compute_emissions_custom, compute_emissions_all, bchart_1, bchart_2
 from flask_cors import CORS, cross_origin #comment this on deployment
 import pandas as pd
@@ -19,35 +19,24 @@ app.config["APPLICATION_ROOT"] = "/"
 def main():
     if request.method == "POST":
 
-        #### NEW FORM ####
-
         if request.form['mode'] == '1' : # My trip vs direct trips
-            # Convert json into pandas
             df = pd.DataFrame.from_dict(json.loads(request.form['my-trip']))
-          #  df.to_csv('query.csv')
-            #My trip data and geo data
+
             data_mytrip, geo_mytrip = compute_emissions_custom(df)
-           # print(data_mytrip)
-            #Direct data and geo data
+
             data_direct, geo_direct = compute_emissions_all(df)
-            #Possible to generate a plotly json, maybe better to plot from the data directly in javascript
-            graph_json, figure = bchart_1(data_mytrip, data_direct)
-           # pd.concat([data_mytrip, data_direct]).to_csv('see_res.csv')
+
             response = {'gdf' : pd.concat([geo_mytrip, geo_direct])[['colors', 'geometry']].explode().to_json(),
                         'my_trip' : data_mytrip.to_json(orient='records'), 'direct_trip': data_direct.to_json(orient='records')}
 
         if request.form['mode'] == '2' : # My trip vs custom trip
-            # Convert json into pandas
             df = pd.DataFrame.from_dict(json.loads(request.form['my-trip']))
             df2 = pd.DataFrame.from_dict(json.loads(request.form['alternative-trip']))
 
-            #My trip data and geo data
             data_mytrip, geo_mytrip = compute_emissions_custom(df)
-            #Direct data and geo data
-            #We change the color to pink
+
             data_alternative, geo_alternative = compute_emissions_custom(df2, cmap = 'RdPu')
-            #Possible to generate a plotly json
-            graph_json, figure = bchart_2(data_mytrip, data_alternative)
+
             response = {'gdf' : pd.concat([geo_mytrip, geo_alternative])[['colors', 'geometry']].explode().to_json(),
                         'my_trip' : data_mytrip.to_json(orient='records'), 'alternative_trip': data_alternative.to_json(orient='records')}
 
