e97580a53e0cb145d78097be9c9eb7308cc564e3
Clara Ni <clara.ni@outlook.fr>
2024-02-08 21:39:21 +0100

    frontend: add navBar

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -9,6 +9,7 @@ import { Map } from "./components/map";
 import { ApiResponse } from "./types";
 import { useSteps } from "./hooks";
 import { API_URL } from "./config";
+import NavBar from "./components/navBar";
 
 function App() {
   const [response, setResponse] = useState<ApiResponse>();
@@ -23,74 +24,79 @@ function App() {
   }, []);
 
   return (
-    <Stack direction="row" className="App" style={{ height: "100vh" }}>
-      <Stack
-        maxWidth="30%"
-        justifyContent="space-between"
-        height="100%"
-        overflow="auto"
-        flexShrink={1}
-      >
-        <Stack padding={3} height="100%">
-          <h1 className="title">Compare the emissions from your travels</h1>
-          <Tabs
-            value={activeTab}
-            onChange={() => setActiveTab((activeTab + 1) % 2)}
-            aria-label="basic tabs example"
-          >
-            <Tab
-              label="My trip"
-              sx={{
-                textTransform: "none",
-                fontFamily: "Montserrat",
-                backgroundColor: activeTab === 0 ? "#efefef" : undefined,
-                borderRadius: activeTab === 0 ? "12px 12px 0 0" : undefined,
-              }}
-            />
-            <Tab
-              label="Alternative trip"
-              sx={{
-                textTransform: "none",
-                fontFamily: "Montserrat",
-                backgroundColor: activeTab === 1 ? "#efefef" : undefined,
-                borderRadius: activeTab === 1 ? "12px 12px 0 0" : undefined,
-              }}
-            />
-          </Tabs>
-          {activeTab === 0 ? (
-            <Form
-              key="main-form"
-              setResponse={setResponse}
-              stepsProps={myTripSteps}
-            />
-          ) : (
-            <Form
-              key="alternative-form"
-              setResponse={setResponse}
-              stepsProps={alternativeTripSteps}
-              stepsToCompare={myTripSteps.values}
-            />
-          )}
-          <Chart response={response} />
-        </Stack>
+    <Stack className="App" style={{ height: "100vh", width: "100vw" }}>
+      <Stack maxHeight={"10%"}>
+        <NavBar></NavBar>
       </Stack>
-      <Container
-        style={{ padding: 0, margin: 0, maxWidth: "100%", flexShrink: 3 }}
-      >
-        <Map
-          response={response}
-          stepsCoords={
-            myTripSteps.values
-              .filter((step) => !!step.locationCoords)
-              .map((step) => step.locationCoords) as [number, number][]
-          }
-          alternativeStepsCoords={
-            alternativeTripSteps.values
-              .filter((step) => !!step.locationCoords)
-              .map((step) => step.locationCoords) as [number, number][]
-          }
-        />
-      </Container>
+      <Stack direction="row" flexGrow={3}>
+        <Stack
+          maxWidth="30%"
+          justifyContent="space-between"
+          height="100%"
+          overflow="auto"
+          flexShrink={1}
+        >
+          <Stack padding={3} height="100%">
+            <h1 className="title">Compare the emissions from your travels</h1>
+            <Tabs
+              value={activeTab}
+              onChange={() => setActiveTab((activeTab + 1) % 2)}
+              aria-label="basic tabs example"
+            >
+              <Tab
+                label="My trip"
+                sx={{
+                  textTransform: "none",
+                  fontFamily: "Montserrat",
+                  backgroundColor: activeTab === 0 ? "#efefef" : undefined,
+                  borderRadius: activeTab === 0 ? "12px 12px 0 0" : undefined,
+                }}
+              />
+              <Tab
+                label="Other trip"
+                sx={{
+                  textTransform: "none",
+                  fontFamily: "Montserrat",
+                  backgroundColor: activeTab === 1 ? "#efefef" : undefined,
+                  borderRadius: activeTab === 1 ? "12px 12px 0 0" : undefined,
+                }}
+              />
+            </Tabs>
+            {activeTab === 0 ? (
+              <Form
+                key="main-form"
+                setResponse={setResponse}
+                stepsProps={myTripSteps}
+              />
+            ) : (
+              <Form
+                key="alternative-form"
+                setResponse={setResponse}
+                stepsProps={alternativeTripSteps}
+                stepsToCompare={myTripSteps.values}
+              />
+            )}
+            <Chart response={response} />
+          </Stack>
+        </Stack>
+        <Container
+          style={{ padding: 0, margin: 0, maxWidth: "100%", flexShrink: 3 }}
+        >
+          <Map
+            response={response}
+            stepsCoords={
+              myTripSteps.values
+                .filter((step) => !!step.locationCoords)
+                .map((step) => step.locationCoords) as [number, number][]
+            }
+            alternativeStepsCoords={
+              alternativeTripSteps.values
+                .filter((step) => !!step.locationCoords)
+                .map((step) => step.locationCoords) as [number, number][]
+            }
+          />
+        </Container>
+      </Stack>
     </Stack>
   );
 }
