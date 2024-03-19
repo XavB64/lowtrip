c6d5f3a09eb70e5d9160a64544fab40446b8cb7e
Clara Ni <clara.ni@outlook.fr>
2024-03-19 09:05:10 +0100

    front: add router and create main view

---
+++ frontend/src/views/errorView.tsx
@@ -0,0 +1,32 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+const ErrorView = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = "Unknown error";
+  }
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage}</i>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorView;
---
+++ frontend/src/views/mainView.tsx
@@ -0,0 +1,61 @@
+import { Box, IconButton, Stack } from "@chakra-ui/react";
+import { useState } from "react";
+import { BiChevronUp } from "react-icons/bi";
+
+import { FormPanel } from "../components/form-panel";
+import { useSteps } from "../hooks";
+import { ApiResponse } from "../types";
+import { Map } from "../components/map";
+import { checkIsOnMobile } from "../utils";
+
+const MainView = () => {
+  const [response, setResponse] = useState<ApiResponse>();
+  const myTripSteps = useSteps();
+  const alternativeTripSteps = useSteps();
+
+  const isOnMobile = checkIsOnMobile();
+
+  return (
+    <>
+      <FormPanel
+        response={response}
+        setResponse={setResponse}
+        myTripSteps={myTripSteps}
+        alternativeTripSteps={alternativeTripSteps}
+      />
+      <Box w="100%" h={["calc(100vh - 64px)", "100%"]}>
+        <Map
+          response={response}
+          stepsCoords={
+            myTripSteps.values
+              .filter((step) => !!step.locationCoords)
+              .map((step) => step.locationCoords) as [number, number][]
+          }
+          alternativeStepsCoords={
+            alternativeTripSteps.values
+              .filter((step) => !!step.locationCoords)
+              .map((step) => step.locationCoords) as [number, number][]
+          }
+        />
+      </Box>
+      {isOnMobile && (
+        <IconButton
+          aria-label="scroll-to-top"
+          icon={<BiChevronUp />}
+          onClick={() =>
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+          }
+          zIndex={2}
+          position="fixed"
+          bottom={3}
+          left={3}
+          colorScheme="blue"
+          isRound
+          display={["flex", "none"]}
+        />
+      )}
+    </>
+  );
+};
+
+export default MainView;
