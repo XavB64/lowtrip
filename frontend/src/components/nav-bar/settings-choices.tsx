d5e206bd1559c05355b1f6b05f5f460220a671fe
Clara Ni <clara.ni@outlook.fr>
2024-03-28 22:50:51 +0100

    front: reorganize navbar files and components

---
+++ frontend/src/components/nav-bar/settings-choices.tsx
@@ -0,0 +1,43 @@
+import { Box, Flex, useBreakpoint } from "@chakra-ui/react";
+
+import { useTranslation } from "react-i18next";
+import { PrimaryButton } from "../primary-button";
+
+const SettingsChoices = ({
+  options,
+  onChange,
+  optionIsSelected,
+  translationKey,
+}: {
+  options: string[];
+  onChange: (option: string) => void;
+  optionIsSelected: (option: string) => boolean;
+  translationKey: string;
+}) => {
+  const { t } = useTranslation();
+  const breakpoint = useBreakpoint();
+  return (
+    <Box mb={1}>
+      <Flex align="center" textAlign="center" justifyContent="center">
+        {options.map((option) => {
+          const isSelected = optionIsSelected(option);
+          return (
+            <PrimaryButton
+              marginRight={1}
+              fontSize={breakpoint === "base" ? 14 : 16}
+              onClick={() => {
+                onChange(option);
+              }}
+              variant={isSelected ? undefined : "outline"}
+              disabled={isSelected}
+            >
+              {t(`navbar.${translationKey}_${option}`)}
+            </PrimaryButton>
+          );
+        })}
+      </Flex>
+    </Box>
+  );
+};
+
+export default SettingsChoices;
