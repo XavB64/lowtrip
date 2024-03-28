33462635a3a8487b5e6a1fa76e3bebd14196b7da
unknown <xbonnema@LSCE.IPSL.FR>
2024-03-27 21:58:35 -0700

    Add copyright

--- frontend/src/components/step-field.tsx
+++ frontend/src/components/step-field.tsx
@@ -1,3 +1,20 @@
+// // Lowtrip, a web interface to compute travel CO2eq for different means of transport worldwide.
+
+// // Copyright (C) 2024  Bonnemaizon Xavier, Ni Clara, Gres Paola & Pellas Chiara
+
+// // This program is free software: you can redistribute it and/or modify
+// // it under the terms of the GNU General Public License as published by
+// // the Free Software Foundation, either version 3 of the License, or
+// // (at your option) any later version.
+
+// // This program is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// // GNU General Public License for more details.
+
+// // You should have received a copy of the GNU General Public License
+// // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { useEffect, useRef, useState } from "react";
 import {
   BiSolidBus,
