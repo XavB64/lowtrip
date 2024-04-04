c10dc40538bf0e57b2e8c72883f4905567284fd7
XavB64 <xavier.bonnemaizon@hotmail.fr>
2024-04-03 19:56:09 -0700

    Add logos and a little bit of text

--- frontend/src/views/about-view.tsx
+++ frontend/src/views/about-view.tsx
@@ -4,16 +4,19 @@ import {
   Tooltip as ChakraTooltip,
   Flex,
   useDisclosure,
+  Image,
+  Link,
   Text,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import MethodologyPdf from "../assets/lowtrip_methodology.pdf";
-
-
+import mollow from "../assets/logo_mollow.png";
+import sailcoop from "../assets/logo_sailcoop.jpg";
 
 const AboutView = () => {
   const { t } = useTranslation();
+
+  const breakpoint = useBreakpoint()
 
 
   return (
@@ -35,11 +38,28 @@ const AboutView = () => {
         textAlign="left"
         justifyContent="center"
       > */}
-        <Text marginBottom={3}>
-        Hello, here we will put general information on how to do slow travel and give examples:
-        Mollow, Sailcoop, Shimla, BonPote...
-        </Text>
 
+        <Text marginBottom={3}>
+        Traveling without using car or airplane (the most carbon intensive means of transport) might be challenging.
+        The train (or bus) network is not uniformised within Europe and finding the right, optimal and affordable connections through countries is a challenge.
+        </Text>
+        <Text marginBottom={3}>
+        The web platform  {' '}<a href={'https://www.mollow.eu/'} target="_blank" rel="noreferrer" style={{color: "blue"}}>Mollow</a>{' '} gives a lot of advices and tested routes to give you inspiration for your next trip.
+        It is built around the idea that the journey itself should be a part your holidays, and takes advantages of this by visiting the wonderful cities you get into along the way.
+        </Text>
+        <Link to='https://www.mollow.eu/' style={{ height: "100%" }}>
+        <Image src={mollow} h={breakpoint === "base" ? 6 :  "60%"} />
+        </Link>
+        <Text marginBottom={3}>
+        Accessing islands without plane require most of the time to take a ferry and this is also a carbon intensive mean of transportation (taken per km).
+        To propose an alternative to that the company sailcoop offers to sail between the continent and corsica for instance
+        </Text>
+        <Link to='https://www.sailcoop.fr/' style={{ height: "100%" }}>
+        <Image src={sailcoop} h={breakpoint === "base" ? 6 :  "60%"} />
+        </Link>
+        <Text marginBottom={3}>
+       We also add the opportunity to give adives on long distance slow traveling with the media BonPote and the Shimla project:
+        </Text>
     </Box>
   );
 };
