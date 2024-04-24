c4ae3733dd150fd6bb3821aa0f7036041a7936c0
François Lorne <francoisl@theodo.fr>
2024-04-24 21:19:10 +0200

    refactor: group loops for creating json

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,24 +1,26 @@
 import { Step } from "./types";
 
-export const formatStepsForApi = (steps: Step[]) => {
-  const lon = steps.reduce((jsonResult, step, index) => {
-    jsonResult[index.toString()] = step.locationCoords![1].toString();
-    return jsonResult;
-  }, {} as { [key: string]: string });
-  const lat = steps.reduce((jsonResult, step, index) => {
-    jsonResult[index.toString()] = step.locationCoords![0].toString();
-    return jsonResult;
-  }, {} as { [key: string]: string });
-  const transp = steps.reduce((jsonResult, step, index) => {
-    jsonResult[index.toString()] = step.transportMean ?? "";
-    return jsonResult;
-  }, {} as { [key: string]: string });
-  const nb = steps.reduce((jsonResult, step, index) => {
-    jsonResult[index.toString()] = step.passengers ?? 1;
-    return jsonResult;
-  }, {} as { [key: string]: number | string });
-  return { lon, lat, transp, nb };
-};
+interface StepsForApi {
+  lon: { [key: string]: string };
+  lat: { [key: string]: string };
+  transp: { [key: string]: string };
+  nb: { [key: string]: number | string };
+}
+
+export const formatStepsForApi = (steps: Step[]): StepsForApi =>
+  steps.reduce(
+    (acc, step, i) => {
+      const index = i.toString();
+      if (step.locationCoords) {
+        acc.lon[index] = step.locationCoords[1].toString();
+        acc.lat[index] = step.locationCoords[0].toString();
+      }
+      acc.transp[index] = step.transportMean ?? "";
+      acc.nb[index] = step.passengers ?? 1;
+      return acc;
+    },
+    { lon: {}, lat: {}, transp: {}, nb: {} } as StepsForApi
+  );
 
 export const checkIsOnMobile = () => {
   return window.innerWidth < 600;
