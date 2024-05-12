d40f1799e048057edeeae528e27187c345f4c2d8
BaudouinH <45173129+BaudouinH@users.noreply.github.com>
2024-05-12 23:15:41 +0200

    Review

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL ?? "";
+export const API_URL = process.env.REACT_APP_API_URL ?? "http://localhost:8000";
 export const PHOTON_API_URL =
   process.env.PHOTON_API_URL ?? "https://photon.komoot.io";
 export const EMAIL_API_SERVICE_KEY =
