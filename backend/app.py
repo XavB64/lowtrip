b133ddc274b5f337d18475409fa4131f5f7eb3a3
Mikael Arguedas <mikael.arguedas@gmail.com>
2024-05-14 06:38:38 +0200

    [back][style] Interested in applying ruff format? (#56)
    * apply ruff format --preview
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>
    
    * typos
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>
    
    * add ruff workflow (with only format for now
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>
    
    * fix format after rebase
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>
    
    * fixup post rebase
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>
    
    ---------
    
    Signed-off-by: Mikael Arguedas <mikael.arguedas@gmail.com>

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,22 @@
-# #     Lowtrip, a web interface to compute travel CO2eq for different means of transport worldwide.
+# Lowtrip, a web interface to compute travel CO2eq for different means of transport worldwide.
 
-# #     Copyright (C) 2024  Bonnemaizon Xavier, Ni Clara, Gres Paola & Pellas Chiara
+# Copyright (C) 2024  Bonnemaizon Xavier, Ni Clara, Gres Paola & Pellas Chiara
 
-    # # This program is free software: you can redistribute it and/or modify
-    # # it under the terms of the GNU Affero General Public License as published
-    # # by the Free Software Foundation, either version 3 of the License, or
-    # # (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-    # # This program is distributed in the hope that it will be useful,
-    # # but WITHOUT ANY WARRANTY; without even the implied warranty of
-    # # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    # # GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 
-    # # You should have received a copy of the GNU Affero General Public License
-    # # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # Librairies
@@ -26,7 +27,7 @@ from backend import (
     chart_refactor,
 )
 from parameters import (
-        colors_alternative,
+    colors_alternative,
 )
 from flask_cors import CORS  # comment this on deployment
 import pandas as pd
@@ -43,7 +44,6 @@ app.config["APPLICATION_ROOT"] = "/"
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == "POST":
-
         #### NEW FORM ####
 
         if request.form["mode"] == "1":  # My trip vs direct trips
@@ -51,43 +51,41 @@ def main():
             df = pd.DataFrame.from_dict(json.loads(request.form["my-trip"]))
             # My trip data and geo data
             data_mytrip, geo_mytrip, error = compute_emissions_custom(df)
-            
-            #Error formatting
+
+            # Error formatting
             if len(error) > 0:
-                error = 'My trip: '+error
-                
-            if not df.shape[0] > 2 :
+                error = "My trip: " + error
+
+            if not df.shape[0] > 2:
                 # Direct data and geo data
                 return_direct = True
                 data_direct, geo_direct = compute_emissions_all(df)
                 # Could be cool to add a message but the error stops the computation every time
                 # if data_direct.shape[0] == 0:
                 #     error += ' Sorry, we could not find other means of transport to compare your trip with.'
-            else :
+            else:
                 return_direct = False
                 data_direct, geo_direct = pd.DataFrame(), pd.DataFrame()
-                
+
                 # Prepare data for aggregation in the chart -  see frontend
                 data_mytrip = chart_refactor(data_mytrip)
-            
 
-            
-            #Check if gdf is empty
+            # Check if gdf is empty
             gdf = pd.concat([geo_direct, geo_mytrip])
             if gdf.shape[0] == 0:
                 gdf = None
-            else :
-                gdf = gdf.explode().to_json() #[l_geo]
-            
-            if return_direct :
-            # Response
+            else:
+                gdf = gdf.explode().to_json()  # [l_geo]
+
+            if return_direct:
+                # Response
                 response = {
                     "gdf": gdf,
                     "my_trip": data_mytrip.to_json(orient="records"),
                     "direct_trip": data_direct.to_json(orient="records"),
                     "error": error,
                 }
-            else :
+            else:
                 response = {
                     "gdf": gdf,
                     "my_trip": data_mytrip.to_json(orient="records"),
@@ -103,23 +101,25 @@ def main():
             data_mytrip, geo_mytrip, error = compute_emissions_custom(df)
             # Error message
             if len(error) > 0:
-                error = 'My trip: '+error
+                error = "My trip: " + error
             # Direct data and geo data
             # We change the color to pink
             data_alternative, geo_alternative, error_other = compute_emissions_custom(
-                df2, 
-                cmap=colors_alternative
+                df2,
+                cmap=colors_alternative,
             )
             # Error message
             if len(error_other) > 0:
-                error_other = 'Other trip: ' + error_other
-                
-            #Check if we have geo data :
+                error_other = "Other trip: " + error_other
+
+            # Check if we have geo data :
             if (len(error) > 0) & (len(error_other) > 0):
                 gdf = None
-            else :
-                gdf = pd.concat([geo_mytrip, geo_alternative]).explode().to_json() #[l_geo]
-           
+            else:
+                gdf = (
+                    pd.concat([geo_mytrip, geo_alternative]).explode().to_json()
+                )  # [l_geo]
+
             # Prepare data for aggregation in the chart -  see frontend
             data_mytrip, data_alternative = chart_refactor(
                 data_mytrip, data_alternative, True
