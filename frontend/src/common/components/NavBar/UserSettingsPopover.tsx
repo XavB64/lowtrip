c13609c70e902e48ce2c396048278139af82e697
Clara Ni <clara.ni@outlook.fr>
2026-04-03 15:16:26 +0900

    🧹 front: rename NavBar folder and its files
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- frontend/src/common/components/NavBar/UserSettingsPopover.tsx
+++ frontend/src/common/components/NavBar/UserSettingsPopover.tsx
@@ -20,7 +20,7 @@ import { useEffect, useRef, useState } from "react";
 import i18n from "i18next";
 import { IoMdSettings } from "react-icons/io";
 
-import SettingsChoices, { LANGUAGES, MAP_THEMES } from "./settings-choices";
+import SettingsChoices, { LANGUAGES, MAP_THEMES } from "./SettingsChoices";
 import "./UserSettings.scss";
 
 const UserSettingsSelector = ({
