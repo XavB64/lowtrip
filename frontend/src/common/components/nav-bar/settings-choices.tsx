5fdc62c7f0b837596b1ba24e8979be1f50bbf3b7
Clara Ni <clara.ni@outlook.fr>
2026-04-03 14:59:36 +0900

    🧹 front: drop ChakraUI in UserSettings
    Part of the removal of ChakraUI.
    
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- frontend/src/common/components/nav-bar/settings-choices.tsx
+++ frontend/src/common/components/nav-bar/settings-choices.tsx
@@ -1,8 +1,24 @@
-import { Box, Flex, useBreakpoint } from "@chakra-ui/react";
+// Lowtrip, a web interface to compute travel CO2eq for different means of transport worldwide.
+
+// Copyright (C) 2024  Bonnemaizon Xavier, Ni Clara, Gres Paola & Pellas Chiara
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { FR, GB, IT, ES, DE } from "country-flag-icons/react/3x2";
 import { useTranslation } from "react-i18next";
 
-import { PrimaryButton } from "../primary-button";
+import Button from "../Button";
 
 export const LANGUAGES = ["fr", "en", "it", "es", "de"];
 export const MAP_THEMES = ["light", "dark"];
@@ -10,18 +26,25 @@ export const MAP_THEMES = ["light", "dark"];
 const getFlag = (language: string) => {
   switch (language) {
     case "en":
-      return <GB style={{ width: "50px" }} />;
+      return <GB className="flag-icon" />;
     case "fr":
-      return <FR style={{ width: "50px" }} />;
+      return <FR className="flag-icon" />;
     case "it":
-      return <IT style={{ width: "50px" }} />;
+      return <IT className="flag-icon" />;
     case "es":
-      return <ES style={{ width: "50px" }} />;
+      return <ES className="flag-icon" />;
     case "de":
-      return <DE style={{ width: "50px" }} />;
+      return <DE className="flag-icon" />;
     default:
       return null;
   }
+};
+
+type SettingsChoicesProps = {
+  options: string[];
+  onChange: (option: string) => void;
+  optionIsSelected: (option: string) => boolean;
+  isLanguageSelector?: boolean;
 };
 
 const SettingsChoices = ({
@@ -29,39 +52,28 @@ const SettingsChoices = ({
   onChange,
   optionIsSelected,
   isLanguageSelector,
-}: {
-  options: string[];
-  onChange: (option: string) => void;
-  optionIsSelected: (option: string) => boolean;
-  isLanguageSelector?: boolean;
-}) => {
+}: SettingsChoicesProps) => {
   const { t } = useTranslation();
-  const breakpoint = useBreakpoint();
   return (
-    <Box mb={1}>
-      <Flex align="center" textAlign="center" justifyContent="center">
-        {options.map((option) => {
-          const isSelected = optionIsSelected(option);
-          const content = isLanguageSelector
-            ? getFlag(option)
-            : t(`navbar.${option}`);
-          return (
-            <PrimaryButton
-              key={option}
-              marginRight={1}
-              fontSize={breakpoint === "base" ? 14 : 16}
-              onClick={() => {
-                onChange(option);
-              }}
-              variant={isSelected ? undefined : "outline"}
-              disabled={isSelected}
-            >
-              {content}
-            </PrimaryButton>
-          );
-        })}
-      </Flex>
-    </Box>
+    <div className="settings-group">
+      {options.map((option) => {
+        const isSelected = optionIsSelected(option);
+        const content = isLanguageSelector
+          ? getFlag(option)
+          : t(`navbar.${option}`);
+
+        return (
+          <Button
+            key={option}
+            className={`settings-button ${isSelected ? "selected" : "outline"}`}
+            onClick={() => onChange(option)}
+            disabled={isSelected}
+          >
+            {content}
+          </Button>
+        );
+      })}
+    </div>
   );
 };
 
--- frontend/src/common/components/nav-bar/user-settings-selector.tsx
+++ frontend/src/common/components/nav-bar/user-settings-selector.tsx
@@ -1,16 +1,27 @@
-import {
-  Button,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverContent,
-  PopoverTrigger,
-  useBreakpoint,
-} from "@chakra-ui/react";
+// Lowtrip, a web interface to compute travel CO2eq for different means of transport worldwide.
+
+// Copyright (C) 2024  Bonnemaizon Xavier, Ni Clara, Gres Paola & Pellas Chiara
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { useEffect, useRef, useState } from "react";
+
 import i18n from "i18next";
 import { IoMdSettings } from "react-icons/io";
 
 import SettingsChoices, { LANGUAGES, MAP_THEMES } from "./settings-choices";
+import "./UserSettings.scss";
 
 const UserSettingsSelector = ({
   themeSettings: { isDarkTheme, switchMapTheme },
@@ -20,45 +31,65 @@ const UserSettingsSelector = ({
     switchMapTheme: () => void;
   };
 }) => {
-  const breakpoint = useBreakpoint();
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopover = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popoverRef.current &&
+        !popoverRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) document.addEventListener("mousedown", handleClickOutside);
+    else document.removeEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <Popover placement="bottom">
-      <PopoverTrigger>
-        <Button
-          borderRadius="15px"
-          fontSize={breakpoint === "base" ? 9 : 16}
-          colorScheme="transparent"
-        >
-          <IoMdSettings size={25} color="white" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent
-        color="white"
-        bg="#efefef"
-        borderColor="#efefef"
-        borderRadius="15px"
-      >
-        <PopoverArrow bg="#efefef" />
-        <PopoverBody>
-          <SettingsChoices
-            options={LANGUAGES}
-            onChange={(language) => i18n.changeLanguage(language)}
-            optionIsSelected={(language) => i18n.language === language}
-            isLanguageSelector
-          />
-          <SettingsChoices
-            options={MAP_THEMES}
-            onChange={() => {
-              switchMapTheme();
-            }}
-            optionIsSelected={(theme) =>
-              (isDarkTheme && theme === "dark") ||
-              (!isDarkTheme && theme === "light")
-            }
-          />
-        </PopoverBody>
-      </PopoverContent>
-    </Popover>
+    <div ref={popoverRef} className="user-settings user-settings-popover">
+      <button className="popover__trigger" onClick={togglePopover}>
+        <IoMdSettings className="setting-icon" />
+      </button>
+
+      {isOpen && (
+        <div className="popover__content">
+          <div className="popover__arrow" />
+
+          <div className="popover__body">
+            <SettingsChoices
+              options={LANGUAGES}
+              onChange={(language) => {
+                i18n.changeLanguage(language);
+                setIsOpen(false);
+              }}
+              optionIsSelected={(language) => i18n.language === language}
+              isLanguageSelector
+            />
+
+            <SettingsChoices
+              options={MAP_THEMES}
+              onChange={switchMapTheme}
+              optionIsSelected={(theme) =>
+                (isDarkTheme && theme === "dark") ||
+                (!isDarkTheme && theme === "light")
+              }
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
